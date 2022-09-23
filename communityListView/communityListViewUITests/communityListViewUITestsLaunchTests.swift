d43faed38444ceccd41d60e03eddb070d3ba42d1
gyeom0919 <leegyeom09@gmail.com>
2022-09-23 23:36:37 +0900

    커뮤니티 리스트 목록 뷰

---
+++ communityListView/communityListViewUITests/communityListViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  communityListViewUITestsLaunchTests.swift
+//  communityListViewUITests
+//
+//  Created by 겸 on 2022/09/23.
+//
+
+import XCTest
+
+final class communityListViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
