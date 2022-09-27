573f47ebb19ee0cc5ab2806be82401e713937456
gyeom0919 <leegyeom09@gmail.com>
2022-09-27 20:35:08 +0900

    Btn Action to Code

--- TermsOfServiceView/TermsOfServiceView/ViewController.swift
+++ TermsOfServiceView/TermsOfServiceView/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonForAll: checkBoxButton!
+    @IBOutlet weak var button1: checkBoxButton!
+    @IBOutlet weak var button2: checkBoxButton!
+    @IBOutlet weak var button3: checkBoxButton!
+    @IBOutlet weak var button4: checkBoxButton!
+    
+    var flagAll = false
+    var flag1 = false
+    var flag2 = false
+    var flag3 = false
+    var flag4 = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func checkAllButton(_ sender: Any) {
+    }
+    
+    @IBAction func checkBoxBtn1(_ sender: Any) {
+    }
+    
+    @IBAction func checkBoxBtn2(_ sender: Any) {
+    }
+    
+    @IBAction func checkBoxBtn3(_ sender: Any) {
+    }
+    
+    @IBAction func checkBoxBtn4(_ sender: Any) {
+    }
+    
+    
+    
 }
+
+
 
